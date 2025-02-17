3809259e3caa499f239be813b38b740d4c28ee19
Diego Cividini <diego.cividini@minidev.it>
2025-02-17 15:29:52 +0100

    initial files

---
+++ Lisp/huffman-codes.lisp
@@ -0,0 +1,1 @@
+;;;; Carbutti Lucia, Cividini Diego
---
+++ Prolog/huffman-codes.pl
@@ -0,0 +1,8 @@
+% Carbutti Lucia, Cividini Diego
+
+%% hucodec_decode/3 Bits HuffmanTree Message
+%% hucodec_encode/3 Message HuffmanTree Bits
+%% hucodec_encode_file/3 Filename HuffmanTree Bits
+%% hucodec_generate_huffman_tree/2 SymbolsAndWeights HuffmanTree
+%% hucodec_generate_symbol_bits_table/2 HuffmanTree SymbolBitsTable
+%% hucodec_print_huffman_tree/1 HuffmanTree
