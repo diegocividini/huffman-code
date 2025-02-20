e3bd5814d15d24ccc18eeff1e928ab5b63a9300c
Diego Cividini <diego.cividini@minidev.it>
2025-02-20 11:38:31 +0100

    working with emacs format

--- Prolog/huffman-codes.pl
+++ Prolog/huffman-codes.pl
@@ -20,8 +20,8 @@ count_occurrences(Char, [OtherChar|Rest], Count) :-
 % Generate a list of symbols and their weights from a list of characters
 % generate_symbols_and_weights/3 Chars Seen SymbolsAndWeights
 generate_symbols_and_weights([], _, []).
-    generate_symbols_and_weights([Char|Chars], Seen,
-				 [sw(Char, Count)|SymbolsAndWeights]) :-
+generate_symbols_and_weights([Char|Chars], Seen,
+			     [sw(Char, Count)|SymbolsAndWeights]) :-
     \+ member(Char, Seen),
     count_occurrences(Char, [Char|Chars], Count),
     generate_symbols_and_weights(Chars, [Char|Seen], SymbolsAndWeights).
@@ -32,11 +32,11 @@ generate_symbols_and_weights([Char|Chars], Seen, SymbolsAndWeights) :-
 % Predicate to insert an element into a sorted list
 % insert_sorted/3 Element List SortedList
 insert_sorted(sw(Char, Count), [], [sw(Char, Count)]).
-    insert_sorted(sw(Char, Count), [sw(Char1, Count1)|Rest],
-		  [sw(Char, Count), sw(Char1, Count1)|Rest]) :-
+insert_sorted(sw(Char, Count), [sw(Char1, Count1)|Rest],
+	      [sw(Char, Count), sw(Char1, Count1)|Rest]) :-
     Count =< Count1.
-    insert_sorted(sw(Char, Count), [sw(Char1, Count1)|Rest],
-		  [sw(Char1, Count1)|SortedRest]) :-
+insert_sorted(sw(Char, Count), [sw(Char1, Count1)|Rest],
+	      [sw(Char1, Count1)|SortedRest]) :-
     Count > Count1,
     insert_sorted(sw(Char, Count), Rest, SortedRest).
 
@@ -47,7 +47,7 @@ insertion_sort([Head|Tail], SortedList) :-
     insertion_sort(Tail, SortedTail),
     insert_sorted(Head, SortedTail, SortedList).
 
-% Main predicate to generate a list of symbols and their weights 
+% Main predicate to generate a list of symbols and their weights and sort it
 % generate_symbols_and_weights_list/2 Text SortedSymbolsAndWeights
 generate_symbols_and_weights_list(Text, SortedSymbolsAndWeights) :-
     string_chars(Text, Chars),
@@ -57,20 +57,21 @@ generate_symbols_and_weights_list(Text, SortedSymbolsAndWeights) :-
 % Predicate to create leaf nodes from symbols and weights
 % create_leaf_nodes/2 SymbolsAndWeights LeafNodes
 create_leaf_nodes([], []).
-    create_leaf_nodes([sw(Char, Count)|Rest],
-		      [node([Char], Count, nil, nil)|LeafNodes]) :-
+create_leaf_nodes([sw(Char, Count)|Rest],
+		  [node([Char], Count, nil, nil)|LeafNodes]) :-
     create_leaf_nodes(Rest, LeafNodes).
 
 % Predicate to combine two nodes
+% combine_nodes/3 Node1 Node2 CombinedNode
 combine_nodes(node(Symbols1, Weight1, Left1, Right1),
 	      node(Symbols2, Weight2, Left2, Right2),
-	      node(Symbols, Weight,
-		   node(Symbols1, Weight1, Left1,
-			node(Symbols2, Weight2, Left2, Right2))) :-
+	      node(Symbols, Weight, node(Symbols1, Weight1, Left1, Right1),
+		   node(Symbols2, Weight2, Left2, Right2))) :-
     append(Symbols1, Symbols2, Symbols),
     Weight is Weight1 + Weight2.
 
 % Predicate to get the weight of a node
+% node_weight/2 Node Weight
 node_weight(node(_, Weight, _, _), Weight).
 
 % Predicate to insert a node into a sorted list of nodes
@@ -80,7 +81,6 @@ insert_node_sorted(Node, [Node1|Rest], [Node, Node1|Rest]) :-
     node_weight(Node, Weight),
     node_weight(Node1, Weight1),
     Weight =< Weight1.
-
 insert_node_sorted(Node, [Node1|Rest], [Node1|SortedRest]) :-
     node_weight(Node, Weight),
     node_weight(Node1, Weight1),
@@ -95,7 +95,7 @@ build_huffman_tree([Node1, Node2|Rest], HuffmanTree) :-
     insert_node_sorted(CombinedNode, Rest, NewNodes),
     build_huffman_tree(NewNodes, HuffmanTree).
 
-% Main predicate to generate the Huffman tree 
+% Main predicate to generate the Huffman tree from a list of symbols and weights
 % hucodec_generate_huffman_tree/2 SymbolsAndWeights HuffmanTree
 hucodec_generate_huffman_tree(SortedSymbolsAndWeights, HuffmanTree) :-
     create_leaf_nodes(SortedSymbolsAndWeights, LeafNodes),
@@ -112,7 +112,7 @@ traverse_huffman_tree(node(_, _, Left, Right), Bits, SymbolBitsTable) :-
     append(LeftTable, RightTable, SymbolBitsTable).
 
 % Predicate to generate the symbol bits table from the Huffman tree
-% hucodec_generate_symbol_bits_table/2 HuffmanTree generate_symbol_bits_table
+% hucodec_generate_symbol_bits_table/2 HuffmanTree SymbolBitsTable
 hucodec_generate_symbol_bits_table(HuffmanTree, SymbolBitsTable) :-
     traverse_huffman_tree(HuffmanTree, [], SymbolBitsTable).
 
@@ -123,9 +123,9 @@ print_huffman_tree(node(Symbols, Weight, nil, nil), Indent) :-
 print_huffman_tree(node(Symbols, Weight, Left, Right), Indent) :-
     format('~wNode: ~w, Weight: ~w~n', [Indent, Symbols, Weight]),
     atom_concat(Indent, '  ', NewIndent),
-    format('~n~wLeft:~n', [NewIndent]),
+    format('~wLeft:~n', [NewIndent]),
     print_huffman_tree(Left, NewIndent),
-    format('~n~wRight:~n', [NewIndent]),
+    format('~wRight:~n', [NewIndent]),
     print_huffman_tree(Right, NewIndent).
 
 % Main predicate to print the Huffman tree
@@ -158,8 +158,8 @@ hucodec_encode(Message, HuffmanTree, Bits) :-
 % decode_bits/4 Bits HuffmanTree CurrentNode Message
 decode_bits([], node([Symbol], _, nil, nil), _, [Symbol]).
 decode_bits([], _, _, []).
-    decode_bits([Bit|Bits], node([Symbol], _, nil, nil), HuffmanTree,
-		[Symbol|RestMessage]) :-
+decode_bits([Bit|Bits], node([Symbol], _, nil, nil),
+	    HuffmanTree, [Symbol|RestMessage]) :-
     decode_bits([Bit|Bits], HuffmanTree, HuffmanTree, RestMessage).
 decode_bits([0|Bits], node(_, _, Left, _), HuffmanTree, Message) :-
     decode_bits(Bits, Left, HuffmanTree, Message).
